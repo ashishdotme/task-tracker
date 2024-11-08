51d19e0df10740fb052f423341c7fb4473e061ef
Ashish Patel <ashishsushilpatel@gmail.com>
2024-11-08 23:08:22 +0000

    feat: disable exit on failure

--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
 	"os"
 	"reflect"
 	"time"
 
 	"github.com/ncruces/zenity"
+	"golang.org/x/sys/windows"
 )
 
 type Todo struct {
@@ -29,112 +28,114 @@ type Todo struct {
 	UpdatedAt     time.Time   `json:"updatedAt"`
 }
 
+var user32 = windows.NewLazyDLL("user32.dll")
+
+func getForegroundWindow() uintptr {
+	hwnd, _, _ := user32.NewProc("GetForegroundWindow").Call()
+	return hwnd
+}
 func main() {
 
 	var csvFile = getFile()
 	csvwriter := csv.NewWriter(csvFile)
 
 	for true {
-
+		hwnd := getForegroundWindow()
 		// if time is right
-		if float64(rand.Intn(99999999)) < 100 {
 
-			// Get todos
-			todosResponse, err := apiReq("https://api.ashish.me/todos/incomplete", "GET")
-			if err != nil {
-				fmt.Println("[E] Failed to get tasks", err)
-				os.Exit(1)
-			}
+		// Get todos
+		todosResponse, err := apiReq("https://api.ashish.me/todos/incomplete", "GET")
+		if err != nil {
+			fmt.Println("[E] Failed to get tasks", err)
+		}
 
-			// parse response
-			var todos []Todo
-			err = json.Unmarshal([]byte(todosResponse), &todos)
-			if err != nil {
-				fmt.Println("[E] Failed to parse", err)
-				os.Exit(1)
-			}
+		// parse response
+		var todos []Todo
+		err = json.Unmarshal([]byte(todosResponse), &todos)
+		if err != nil {
+			fmt.Println("[E] Failed to parse", err)
+		}
 
-			var chore = Todo{}
-			chore.Category = "Chore"
-			chore.Content = "Household Chore"
+		var chore = Todo{}
+		chore.Category = "Chore"
+		chore.Content = "Household Chore"
 
-			var dev = Todo{}
-			dev.Category = "Programming"
-			dev.Content = "Other coding task"
+		var dev = Todo{}
+		dev.Category = "Programming"
+		dev.Content = "Other coding task"
 
-			var youtube = Todo{}
-			youtube.Category = "Social"
-			youtube.Content = "Youtube"
+		var youtube = Todo{}
+		youtube.Category = "Social"
+		youtube.Content = "Youtube"
 
-			var news = Todo{}
-			news.Category = "Social"
-			news.Content = "News"
+		var news = Todo{}
+		news.Category = "Social"
+		news.Content = "News"
 
-			var fb = Todo{}
-			fb.Category = "Social"
-			fb.Content = "Facebook/Instagram/Discord"
+		var fb = Todo{}
+		fb.Category = "Social"
+		fb.Content = "Facebook/Instagram/Discord"
 
-			todos = append(todos, dev)
-			todos = append(todos, chore)
-			todos = append(todos, youtube)
-			todos = append(todos, news)
-			todos = append(todos, fb)
-			newTask := Todo{
-				Category: "General",
-				Content:  "New task",
-			}
-			todos = append(todos, newTask)
+		todos = append(todos, dev)
+		todos = append(todos, chore)
+		todos = append(todos, youtube)
+		todos = append(todos, news)
+		todos = append(todos, fb)
+		newTask := Todo{
+			Category: "General",
+			Content:  "New task",
+		}
+		todos = append(todos, newTask)
 
-			// Create list
-			var taskArray []string
-			for i := 0; i < len(todos); i++ {
-				taskArray = append(taskArray, todos[i].Content)
-			}
+		// Create list
+		var taskArray []string
+		for i := 0; i < len(todos); i++ {
+			taskArray = append(taskArray, todos[i].Content)
+		}
 
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
-			defer cancel()
+		// ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+		// defer cancel()
 
-			// Show alert
-			task, err := zenity.List(
-				"Select task from the list below:",
-				taskArray[:],
-				zenity.Title("Time tracker"),
-				zenity.CancelLabel("Exit"),
-				zenity.Height(400),
-				zenity.Context(ctx),
-			)
+		// Show alert
+		task, err := zenity.List(
+			"Select task from the list below:",
+			taskArray[:],
+			zenity.Title("Time tracker"),
+			zenity.CancelLabel("Exit"),
+			zenity.Height(400),
+			zenity.Attach(hwnd),
+		)
 
-			if task == "New task" {
-				var newTask, _ = zenity.Entry("Add new task")
-				if err := sendNewTask(newTask, os.Getenv("ASHISHDOTME_TOKEN")); err != nil {
-					fmt.Printf("Error: %v\n", err)
-				} else {
-					fmt.Println("Task sent successfully!")
-				}
-				time.Sleep(240 * time.Second)
+		if task == "New task" {
+			var newTask, _ = zenity.Entry("Add new task")
+			if err := sendNewTask(newTask, os.Getenv("ASHISHDOTME_TOKEN")); err != nil {
+				fmt.Printf("Error: %v\n", err)
 			} else {
-
-				// find task
-				idx := Find(todos, func(value interface{}) bool {
-					return value.(Todo).Content == task
-				})
-
-				// save data in csv file
-				if task != "" && err == nil && idx >= 0 {
-					t, _ := time.Now().UTC().MarshalText()
-					row := []string{string(t), time.Now().Format("01-02-2006"), todos[idx].Content, todos[idx].Category}
-					csvwriter.Write(row)
-				} else {
-					if err == zenity.ErrCanceled {
-						os.Exit(1)
-					}
-					t, _ := time.Now().UTC().MarshalText()
-					row := []string{string(t), time.Now().Format("01-02-2006"), "Timeout", "Timeout"}
-					csvwriter.Write(row)
-				}
-				csvwriter.Flush()
-				time.Sleep(240 * time.Second)
+				fmt.Println("Task sent successfully!")
 			}
+			time.Sleep(8 * time.Minute)
+		} else {
+
+			// find task
+			idx := Find(todos, func(value interface{}) bool {
+				return value.(Todo).Content == task
+			})
+
+			// save data in csv file
+			if task != "" && err == nil && idx >= 0 {
+				t, _ := time.Now().UTC().MarshalText()
+				row := []string{string(t), time.Now().Format("01-02-2006"), todos[idx].Content, todos[idx].Category}
+				csvwriter.Write(row)
+			} else {
+				if err == zenity.ErrCanceled {
+					fmt.Println("Cancelled")
+				}
+				t, _ := time.Now().UTC().MarshalText()
+				row := []string{string(t), time.Now().Format("01-02-2006"), "Timeout", "Timeout"}
+				csvwriter.Write(row)
+			}
+			csvwriter.Flush()
+			time.Sleep(8 * time.Minute)
 		}
 	}
 	csvFile.Close()
